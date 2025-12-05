ec24a3402143a879ee95386070a864e90196d8e1
Wladislaw Repin <repin@mailbox.org>
2025-12-05 19:47:34 +0100

    Firebase eingefügt

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mealtrack/core/config/bootstrap.dart';
 import 'package:mealtrack/core/data/hive_initializer.dart';
 import 'package:mealtrack/features/inventory/presentation/inventory_page.dart';
+import 'package:mealtrack/firebase_options.dart';
+import 'package:mealtrack/example.dart';
 import 'package:mealtrack/shared/widgets/initialization_error_app.dart';
 
 Future<void> main() async {
   // Stellt sicher, dass die Flutter-Engine initialisiert ist, bevor
   // plattformspezifischer Code (z.B. für den Dateisystemzugriff via path_provider) ausgeführt wird.
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final isInitialized = await bootstrap(AppHiveInitializer());
   runApp(isInitialized ? const MealTrack() : const InitializationErrorApp());
 }
