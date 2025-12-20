dab7740352b8d636d494fbba0975f579a5f47f57
Wladislaw Repin <repin@mailbox.org>
2025-12-20 15:22:46 +0100

    so

--- lib/core/config/bootstrap.dart
+++ lib/core/config/bootstrap.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mealtrack/core/data/hive_initializer.dart';
+import 'package:mealtrack/features/inventory/data/fridge_item_repository.dart';
 
 Future<bool> bootstrap(HiveInitializer hiveInitializer) async {
   try {
     await hiveInitializer.init();
+    FridgeItemRepository fridgeItemRepository = FridgeItemRepository();
+    await fridgeItemRepository.init();
 
     return true;
   } catch (e, stackTrace) {
