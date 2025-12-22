8d568649e4e5c54b04fef83dbe44d5451edc7939
repinw <113454907+repinw@users.noreply.github.com>
2025-12-22 14:58:26 +0100

    33 Chore: Integrate Riverpod 3.0 with Code Generation (#41)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mealtrack/app.dart';
 import 'package:mealtrack/firebase_options.dart';
 
@@ -8,5 +9,5 @@ Future<void> main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MealTrackApp());
+  runApp(ProviderScope(child: const MealTrackApp()));
 }
