dd6b8df419327b2f548ac09f252ea94450442885
Wladislaw Repin <repin@mailbox.org>
2026-02-13 19:13:40 +0100

    feat(calories): add core models and repositories

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -7,6 +7,9 @@ const String invitesCollection = 'invites';
 const String householdsCollection = 'households';
 const String shoppingListCollection = 'shopping_list';
 const String categoryStatsCollection = 'category_stats';
+const String calorieLogsCollection = 'calorie_logs';
+const String calorieSettingsCollection = 'calorie_settings';
+const String offProductsCacheCollection = 'off_products_cache';
 const String defaultDateFormat = 'dd.MM.yyyy';
 
 final DateFormat standardDateFormat = DateFormat(defaultDateFormat);
